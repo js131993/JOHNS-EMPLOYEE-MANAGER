ef954f49c571c86a8a979153516e41cf95cb7d38
js131993 <john.sanders4444@gmail.com>
2023-08-01 18:02:50 -0400

    Update seeds.sql
    Updated view employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO departments (department_id, department)
+INSERT INTO departments (department_id, departmentName)
 VALUES (1, " SALES"),
        (2, "ENGINEERING"),
        (3, "FINANCE"),
@@ -11,8 +11,17 @@ VALUES (1, "Sales Lead", 1 , 10),
        (4, "Software Engineer", 2, 45),
        (5, "Account Manager", 3, 50),
        (6, "Accountant", 3, 78),
-       (7, "Legal Team Lead", 4, 32)
-       (8, "Lawyer", 4, 78),
+       (7, "Legal Team Lead", 4, 32),
+       (8, "Lawyer", 4, 78);
 
-INSERT INTO view_all_employees (employees_id, first_name, last_name, title, department
+INSERT INTO view_all_employees (employees_id, firstName, lastName, role_id, manager_id)
+VALUES (1, "John", "Harvard" , 1),
+       (2, "Johnnie", "Bravo" , 3),
+       (3, "Scooby", "Doo" , 3),
+       (4, "William", "Shakespeare" , 7),
+       (5, "Harry", "Potter" , 5),
+       (6, "Ryan", "Gosling" , null),
+       (7, "Will", "Smith" , null),
+       
+
 
