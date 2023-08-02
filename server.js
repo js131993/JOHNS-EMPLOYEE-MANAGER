795c51912ed63d35a76d2e80c012dabf22b78c27
js131993 <john.sanders4444@gmail.com>
2023-08-02 17:19:00 -0400

    Connected to sql database
    Downloaded sql and connected to database.

--- server.js
+++ server.js
@@ -3,6 +3,16 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer")
 
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "db",
+  },
+  console.log(`Connected to the movies_db database.`)
+);
+
 // No middleware because it is not a server application(such as APIs)
 
 //I need this code, runs for every request and response
@@ -11,11 +21,45 @@ const inquirer = require("inquirer")
 const mainMenuQuestions = [
   {
     name: "choicesMainMenu",
-    choices: "list",
+    type: "list",
     message: "What would you like to do?",
-        choices: ["View All Employees", "Add Employee", "Update Employee Role","View All Roles", "Add Role", "View All Departments", "Add Department", "View All Employees","Quit"],
-    },
+    choices: [
+      "View All Employees",
+      "Add Employee",
+      "Update Employee Role",
+      "View All Roles",
+      "Add Role",
+      "View All Departments",
+      "Add Department",
+      "View All Employees",
+      "Quit",
+    ],
+  },
 ]; 
+
+inquirer.prompt(mainMenuQuestions).then((choices) => {
+  console.log(choices.choicesMainMenu)
+  const expr = choices.choicesMainMenu;
+  switch (expr) {
+    case "View All Employees":
+      console.log("view all employess selected");
+
+      break;
+    case "Add Employee": ''
+      break;
+    case "View All Roles":
+      break;
+    case "Add Role":
+      break;
+    case "View All Departments":
+      break;
+    case "Add Department":
+      break;
+    default:
+      console.log(`Sorry, we are out of ${expr}.`);
+  }
+
+});
 // There are a when property for something is true...(look at it later)
 
 //hardcore roles into information/questions
@@ -35,7 +79,15 @@ const addDepartmentQuestion = [
     message: "Would you like to add a department?",
       choices: ["YES", "NO"],
   }
+]
 
+const departmentAdded = [
+  {
+    name: "departmentAdded",
+    type: 'checkbox',
+
+
+  }
 ]
 
 const updateEmployeeRole = [
@@ -46,6 +98,17 @@ const updateEmployeeRole = [
       choices: ["YES", "NO"],
   }
 
+]
+
+// const
+
+const viewAllroles = [
+  {
+    name: "viewAllroles",
+    choices: "list",
+    message: " Would you like to view all roles?",
+      choices: ["YES", "NO"]
+  }
 ]
 
 //when updating you must make a new variable
