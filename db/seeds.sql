c723af65c8fc9c9bd5606d0ec5af6b6f87a387f4
js131993 <john.sanders4444@gmail.com>
2023-08-02 19:42:13 -0400

    removing id
    Took away department id employee id roles id because I made it auto increment in the schema.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,27 @@
-INSERT INTO departments (department_id, departmentName)
-VALUES (1, " SALES"),
-       (2, "ENGINEERING"),
-       (3, "FINANCE"),
-       (4, "LEGAL"),
+INSERT INTO departments (department_name)
+VALUES (" SALES"),
+       ( "ENGINEERING" ),
+       ( "FINANCE" ),
+       ( "LEGAL" ),
 
-INSERT INTO roles (roles_id, title, department_id, salary)
-VALUES (1, "Sales Lead", 1 , 10),
-       (2, "Salesperson", 1, 15),
-       (3, "Lead Engineer", 2, 23),
-       (4, "Software Engineer", 2, 45),
-       (5, "Account Manager", 3, 50),
-       (6, "Accountant", 3, 78),
-       (7, "Legal Team Lead", 4, 32),
-       (8, "Lawyer", 4, 78);
+INSERT INTO roles (title, department_id, salary)
+VALUES ( "Sales Lead", 1 , 10000),
+       ( "Salesperson", 1, 70000),
+       ( "Lead Engineer", 2, 800000),
+       ( "Software Engineer", 2, 75000),
+       ( "Account Manager", 3, 90000),
+       ( "Accountant", 3, 60000),
+       ( "Legal Team Lead", 4, 150000),
+       ( "Lawyer", 4, 170000);
 
-INSERT INTO view_all_employees (employees_id, firstName, lastName, role_id, manager_id)
-VALUES (1, "John", "Harvard" , 1),
-       (2, "Johnnie", "Bravo" , 3),
-       (3, "Scooby", "Doo" , 3),
-       (4, "William", "Shakespeare" , 7),
-       (5, "Harry", "Potter" , 5),
-       (6, "Ryan", "Gosling" , null),
-       (7, "Will", "Smith" , null),
+INSERT INTO view_all_employees (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Harvard" , 1),
+       ("Johnnie", "Bravo" , 3),
+       ("Scooby", "Doo" , 3),
+       ("William", "Shakespeare" , 7),
+       ("Harry", "Potter" , 5),
+       ("Ryan", "Gosling" , null),
+       ("Will", "Smith" , null),
        
 
 
