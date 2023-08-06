21cfe99a2f4db114bf9eb97d48fc2d628ad5430c
js131993 <john.sanders4444@gmail.com>
2023-08-06 19:19:31 -0400

    Update server.js
    updating server and working on bug fixes

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const mysql = require("mysql2");
 const inquirer = require("inquirer");
 
 //create connection only takes one argument (object)
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "db",
-  },
-);
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "db",
+});
 
 // No middleware because it is not a server application(such as APIs)
 
@@ -26,36 +24,18 @@ const mainMenuQuestions = [
       "Add Role",
       "View All Departments",
       "Add Department",
-      "View All Employees",
       "Quit",
     ],
   },
 ];
 //the questions can not be defined in the global scope(move these to their functions)
 
-
-
 const addDepartmentQuestions = [
   {
     name: "department",
     type: "input",
-    message: "Please enter the name of the new department."
+    message: "Please enter the name of the new department.",
   },
-];
-
-const updateEmployeeRoleQuestions = [
-  {
-    name: "pickEmployee",
-    type: "list",
-    message: "Select the employee you would like to update.",
-    choices: [employees],
-  },
-  {
-    name: "updatedRole",
-    type: "list",
-    message: "Select the employee's new role in the company.",
-    choices: [roles],
-  }
 ];
 
 function viewEmployees() {
@@ -70,10 +50,7 @@ function viewEmployees() {
 }
 
 //having trouble building function
-function addEmployee () {
-  // inquirer
-  //   .prompt(addEmployee)
-  //   .then
+function addEmployee() {
   let roles = viewRoles();
   let employees = viewEmployees();
   let addEmployeeQuestions = [
@@ -101,77 +78,103 @@ function addEmployee () {
       //... is a spread operator, taking employees and sticking into array, allowing you to make more choices... it can be null and allow more choices
     },
   ];
+  inquirer
+    .prompt(addEmployeeQuestions)
+    //prompt returns a promise and that's why we can change,
+    //make sure library supports terminology
+    .then((answers) => {
+      //answers will have information for addEmployeeQuestions which will be used for the add employees table
+      /*
+
+      */
+      console.log(answers);
+    });
 }
 
-  function viewRoles() {
-    const sql = "SELECT * FROM roles";
-    db.query(sql, null, (err, result) => {
-      if (err) {
-        console.log("error");
-        return;
-        //need the return for the console.log(error)
-      }
-      return result;
-    });
-  }
-
-    function viewDepartments() {
-      const sql = "SELECT * FROM departments";
-      db.query(sql, params, (err, result) => {
-        if (err) {
-          res.status(400).json({ error: err.message });
-          return;
-        }
-      });
+function viewRoles() {
+  const sql = "SELECT * FROM roles";
+  db.query(sql, null, (err, result) => {
+    if (err) {
+      console.log("error");
+      return;
+      //need the return for the console.log(error)
     }
+    return result;
+  });
+}
+
+function updateEmmployeeRole() {
+  let roles = viewRoles();
+  let employees = viewEmployees();
+  const updateEmployeeRoleQuestions = [
+    {
+      name: "pickEmployee",
+      type: "list",
+      message: "Select the employee you would like to update.",
+      choices: [employees],
+    },
+    {
+      name: "updatedRole",
+      type: "list",
+      message: "Select the employee's new role in the company.",
+      choices: [roles],
+    },
+  ];
+}
+
+function viewDepartments() {
+  const sql = "SELECT * FROM departments";
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+  });
+}
 //calling existing functions
 
-      
-//using switch so that if then does not need to be used constantly in the 
+//using switch so that if then does not need to be used constantly in the
 inquirer.prompt(mainMenuQuestions).then((choices) => {
-  console.log(choices.choicesMainMenu)
   const expr = choices.choicesMainMenu;
   switch (expr) {
     case "View All Employees":
       viewEmployees();
       console.log("View all employees.");
       break;
-  
+
     case "Add Employee":
       addEmployee();
-      console.log("Adding an employee.")
+      console.log("Adding an employee.");
       break;
-    
+
     case "Update Employee Role":
       updateEmployeeRole();
-      console.log("Update employee role.")
+      console.log("Update employee role.");
       break;
-    
+
     case "View All Roles":
-      viewRoles()
-      console.log("View All Roles")
+      viewRoles();
+      console.log("View All Roles");
       break;
-    
+
     case "Add Role":
-      addRole()
-      console.log("Add role to employee")
+      addRole();
+      console.log("Add role to employee");
       break;
-    
+
     case "View All Departments":
-      viewDepartments()
-      console.log("View All Departments")
+      viewDepartments();
+      console.log("View All Departments");
       break;
     //all take in arguments
     case "Add Department":
-      addDepartment()
-      console.log("Adding department.")
+      addDepartment();
+      console.log("Adding department.");
 
       break;
     case "Quit":
-      console.log("You have exited the emmployee database. ");
-      process.exit();
-    default:
-      console.log(`Sorry, we are out of ${expr}.`);
+      break;
   }
-}
-);
+});
+
+//
