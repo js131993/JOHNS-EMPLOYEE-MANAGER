d332e807d306764de6d8f462be1b667482e2e27d
js131993 <john.sanders4444@gmail.com>
2023-08-07 13:14:40 -0400

    seeds->schema
    put information in wrong folder.  Needed information to be in schema for foreign key.

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,10 @@ CREATE TABLE roles(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50),
     department_id INT,
-    salary DECIMAL
+    salary DECIMAL,
+    FOREIGN KEY (department_id),
+    REFERENCES departments(id),
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
@@ -20,5 +23,8 @@ CREATE TABLE employees (
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     role_id VARCHAR(50),
-    manager_id VARCHAR(50)
+    manager_id VARCHAR(50),
+    FOREIGN KEY (role_id),
+    REFERENCES roles(id),
+    ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -12,9 +12,7 @@ VALUES ( "Sales Lead", 1 , 10000),
        ( "Account Manager", 3, 90000),
        ( "Accountant", 3, 60000),
        ( "Legal Team Lead", 4, 150000),
-       ( "Lawyer", 4, 170000),
-       FOREIGN KEY (department_id),
-       REFERENCES departments(id);
+       ( "Lawyer", 4, 170000);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Harvard" , 1, null),
@@ -23,9 +21,7 @@ VALUES ("John", "Harvard" , 1, null),
        ("William", "Shakespeare", 7, 3),
        ("Harry", "Potter" , 5, 2),
        ("Ryan", "Gosling" , 3, 4),
-       ("Will", "Smith" , 5, null),
-       FOREIGN KEY (role_id),
-       REFERENCES roles(id);
+       ("Will", "Smith" , 5, null);
        
 
 
