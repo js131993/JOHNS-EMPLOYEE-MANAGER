44325889fd68ff0419ea87ce35b3d5507b051538
js131993 <john.sanders4444@gmail.com>
2023-08-10 14:26:40 -0400

    Update schema.sql
    changing schema and tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-DROP DATABASE IF EXISTS AllEmployee_db;
+ROP DATABASE IF EXISTS AllEmployee_db;
 CREATE DATABASE AllEmployee_db;
 
 USE AllEmployee_db;
@@ -20,8 +20,7 @@ CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    role_id INT,
-    manager_id INT,
+    role_id VARCHAR(50),
+    manager_id VARCHAR(50),
     FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
-    FOREIGN KEY(manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
