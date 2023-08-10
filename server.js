05aa2c9dabe15c29e004e1f6312f211cd2cd9b1b
js131993 <john.sanders4444@gmail.com>
2023-08-09 20:41:19 -0400

    Update server.js
    adding main questions into switch function and repositioning.

--- server.js
+++ server.js
@@ -21,23 +21,68 @@ mysql
   });
 // No middleware because it is not a server application(such as APIs)
 
-const mainMenuQuestions = [
-  {
-    name: "choicesMainMenu",
-    type: "list",
-    message: "What would you like to do?",
-    choices: [
-      "View All Employees",
-      "Add Employee",
-      "Update Employee Role",
-      "View All Roles",
-      "Add Role",
-      "View All Departments",
-      "Add Department",
-      "Quit",
-    ],
-  },
-];
+async function main(db) {
+  const mainMenuQuestions = [
+    {
+      name: "choicesMainMenu",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Quit",
+      ],
+    },
+  ];
+
+  // Infinite loop until the User selects 'Quit' or force closes the Application.
+  while (true) {
+    let answers = await inquirer.prompt(mainMenuQuestions);
+    switch (answers.choicesMainMenu) {
+      // Get a table of all Employees in the Application.
+      case "View All Employees":
+        let employees = await readEmployees(db);
+        console.table(employees);
+        break;
+      // Add a new Employee to the Application.
+      case "Add Employee":
+        let employee = await promptAddEmployeeQuestions(db);
+        await createEmployee(db, employee);
+        break;
+      // Change an Employee's Role in the Application.
+      case "Update Employee Role":
+        await promptUpdateEmployeeRoleQuestions(db);
+        console.log("Update employee role.");
+        break;
+      // Get a table of all Roles in the Application.
+      case "View All Roles":
+        const roles = await readRoles(db);
+        console.table(roles);
+        break;
+      // Add a new Role to the Application.
+      case "Add Role":
+        await createRole(db);
+        break;
+      // Get a table of all Departments in the Application.
+      case "View All Departments":
+        const departments = await readDepartments(db);
+        console.table(departments);
+        break;
+      // Add a new Department to the Application.
+      case "Add Department":
+        await createDepartment(db);
+        console.log("Adding department.");
+        break;
+      case "Quit":
+        process.exit();
+    }
+  }
+}
 
 //the questions can not be defined in the global scope(move these to their functions)
 async function viewEmployees() {
@@ -187,53 +232,3 @@ let running = true;
 
 
 
-function askMainQuestions() {
-  inquirer
-    .prompt(mainMenuQuestions)
-    .then(async (choices) => {
-      const expr = choices.choicesMainMenu;
-      switch (expr) {
-        case "View All Employees":
-          const employees = await viewEmployees();
-          console.table(employees);
-          break;
-
-        case "Add Employee":
-          addEmployee();
-          console.log("Adding an employee.");
-          break;
-
-        case "Update Employee Role":
-          const updateEmployee = await updateEmployeeRole();
-          console.log(updateEmployee);
-          break;
-
-        case "View All Roles":
-          const roles = await viewRoles();
-          console.table(roles);
-          break;
-
-        case "Add Role":
-          updateEmmployeeRole();
-          console.log("Add role to employee");
-          break;
-
-        //below was changed to variable and called with console.table
-        case "View All Departments":
-          const departments = await viewDepartments();
-          console.table(departments);
-          break;
-        //all take in arguments
-        case "Add Department":
-          addDepartment();
-          console.log("Adding department.");
-          break;
-        case "Quit":
-          process.exit();
-      }
-      askMainQuestions();
-    })
-}
-
-askMainQuestions();
-//use above for any error that
